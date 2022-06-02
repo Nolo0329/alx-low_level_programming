44062e7d9c8801db8301fff03131ae6d16a573fd
Nolo0329 <masemolamahlogonolo@gmail.com>
2022-06-02 14:52:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (Sucess)
  */
 int main(void)
-}
-	puts("\"Programming is like building a multilingual puzzle")
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
