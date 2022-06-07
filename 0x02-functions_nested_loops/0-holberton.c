0aa919c73eed97e126af062eab9254550f6eca76
Nolo0329 <masemolamahlogonolo@gmail.com>
2022-06-07 06:15:37 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
-
 /**
- * main - Entryu point
- * Return: 0
+ * main - Prints Holberton
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char c[10] = "Holberton";
-	int i = 0;
-	while (i < 9)
-	{
-		_putchar(c[i]);
-		i++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 	return (0);
 }
