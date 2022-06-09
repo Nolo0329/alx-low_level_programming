86d44a955b0a807e9045e94871c5a2c4183be60e
Nolo0329 <masemolamahlogonolo@gmail.com>
2022-06-09 12:34:49 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,21 +4,17 @@
  * print_most_numbers - print numbers from 0 - 9 except 2 - 4
  *
  * Return: void
- *
  */
 
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i < 10; i++)
 	{
-		if (i ++ 50 || i ++ 52)
+		if (i != 2 && i != 4)
 		{
-		}
-		else
-		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 	_putchar('\n');
