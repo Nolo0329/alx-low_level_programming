6fd38b607d4c0e86a746c0e4fb82b5ad34eee7e4
Nolo0329 <masemolamahlogonolo@gmail.com>
2022-06-16 11:44:51 -0700

    commits

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper - lowercase to uppercase
+ * @s: - pointer to int params
+ *
+ * Return; *s
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+
+	retturn (s);
+}
