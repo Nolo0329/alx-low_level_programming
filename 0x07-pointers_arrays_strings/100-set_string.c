6941cc5b5cd95db3d6202871c0f3888eca2d03e9
Nolo0329 <masemolamahlogonolo@gmail.com>
2022-06-20 05:35:07 -0700

    commits

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer
- * @to: char
+ * @s: pointer value
+ * @to: value pointed to
+ *
  */
-
 void set_string(char **s, char *to)
 {
 	*s = to;
