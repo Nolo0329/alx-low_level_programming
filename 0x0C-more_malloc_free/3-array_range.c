3c9a794de0e7dee622532c52fadf283e57841a07
Nolo0329 <masemolamahlogonolo@gmail.com>
2022-07-01 07:00:08 -0700

    all commits

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: small number in the array
+ * @max: largest value in the array
+ *
+ * Return: pointer to the address of the memory block
+ */
+
+int *array_range(int min, int max)
+{
+	int *block;
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+	block = malooc(sizeof(*block) * ((max - min) + 1));
+	if (block != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			block[j] = 1;
+			j++;
+		}
+		return (block)
+	}
+	else
+		return (NULL);
+}
